913b2074975abce380ea27e92f1d83c216604ce0
BA <engineervinceblair@gmail.com>
2020-05-09 12:30:49 +0300

    initialized upvote & downvote

--- app/models.py
+++ app/models.py
@@ -13,16 +13,16 @@ class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='lovie.jpeg')
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return f((self.username), (self.email), (self.image_file))
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
 class Post(db.Model):
-    __tablename__ = 'pitch'
+    __tablename__ = 'pitches'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
@@ -30,7 +30,7 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f((self.title), (self.date_posted))
+        return f"Post('{self.title}', '{self.date_posted}')"
     
 class Comment(db.Model):
     __tablename__ = 'comments'
@@ -40,15 +40,15 @@ class Comment(db.Model):
     content = db.Column(db.Text, nullable=False)
     
     def __repr__(self):
-        return f((self.date_posted))
+        return f"User('{self.date_posted}')"
     
-# class Vote(db.Model):
-#     __tablename__ = 'votes'
-#     id = db.Column(db.Integer, primary_key=True)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-#     upvote = db.Column('upvote',db.Integer,default=0)
-# 	downvote = db.Column('downvote',db.Integer,default=0)
+class Vote(db.Model):
+    __tablename__ = 'votes'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    upvote = db.Column('upvote',db.Integer,default=0)
+	downvote = db.Column('downvote',db.Integer,default=0)
 
-#     def __repr__(self):
-#         return f((self.votes))
+    def __repr__(self):
+        return f"User('{self.votes}')"
     
